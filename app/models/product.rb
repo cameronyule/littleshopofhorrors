fc8a6b1f4d3f6cad812f53a3aa7364446fbb5b4f
Cameron Yule <cameron@cameronyule.com>
2016-11-05 20:25:50 +0000

    Product validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,19 @@
 class Product
   include Mongoid::Document
+  include ActiveSupport::NumberHelper
+
   field :name, type: String
   field :description, type: String
   field :price, type: Integer
+
+  validates :name, :description, :price, presence: true
+  validates :price, numericality: {
+    only_integer: true,
+    greater_than: 0
+  }
+
+  def display_price
+    number_to_currency price.to_f/100
+  end
+
 end
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,40 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "should require a name" do
+    p = Product.create description: "test", price: 10
+    assert_not p.valid?
+    assert_not_empty p.errors.details[:name]
+  end
+
+  test "should require a description" do
+    p = Product.create name: "test", price: 10
+    assert_not p.valid?
+    assert_not_empty p.errors.details[:description]
+  end
+
+  test "should require a price" do
+    p = Product.create name: "test", description: "test"
+    assert_not p.valid?
+    assert_not_empty p.errors.details[:price]
+  end
+
+  test "should require a price greater than 0" do
+    p = Product.create name: "test", description: "test", price: 0
+    assert_not p.valid?
+    assert_not_empty p.errors.details[:price]
+  end
+
+  test "should validate with correct parameters" do
+    p = Product.create name: "test", description: "test", price: 1
+    assert p.valid?
+    assert_empty p.errors
+  end
+
+  test "should format price for display" do
+    p = Product.create name: "test", description: "test", price: 5273
+    assert_equal "£52.73", p.display_price
+  end
+
 end
