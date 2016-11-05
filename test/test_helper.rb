05ac31bb4687ff267c9f9bddc61ca13478467c00
Cameron Yule <cameron@cameronyule.com>
2016-11-05 17:02:33 +0000

    Configuring DatabaseCleaner
    Ensuring our test environment database is cleaned after each test case has
    been run, to ensure tests are operating in isolation.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,19 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require 'database_cleaner'
 
 class ActiveSupport::TestCase
   # Add more helper methods to be used by all tests here...
   include FactoryGirl::Syntax::Methods
+
+  DatabaseCleaner.strategy = :truncation
+
+  setup do
+    DatabaseCleaner.start
+  end
+
+  teardown do
+    DatabaseCleaner.clean
+  end
 end
