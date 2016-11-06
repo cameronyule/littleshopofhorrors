77e69dabb0902c24f6a3a0b1b628f445f3215bbb
Cameron Yule <cameron@cameronyule.com>
2016-11-06 12:12:22 +0000

    Calculate basket total

--- app/models/basket.rb
+++ app/models/basket.rb
@@ -2,4 +2,8 @@ class Basket
   include Mongoid::Document
   embedded_in :shopper
   has_and_belongs_to_many :products, inverse_of: nil
+
+  def total
+    products.reduce(0) {|acc, p| acc += p.price }
+  end
 end
--- test/models/basket_test.rb
+++ test/models/basket_test.rb
@@ -3,8 +3,16 @@ require 'test_helper'
 class BasketTest < ActiveSupport::TestCase
 
   test "should contain an empty products array after initialisation" do
-    basket = Basket.new
+    basket = build(:basket)
     assert_empty basket.products
+  end
+
+  test "should calculate the total price of products contained" do
+    basket = build(:basket)
+    basket.products << build(:product, price: 100)
+    basket.products << build(:product, price: 200)
+    basket.products << build(:product, price: 300)
+    assert_equal 600, basket.total
   end
 
 end
