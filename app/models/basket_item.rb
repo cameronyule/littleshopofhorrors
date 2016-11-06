780f611b3091f094ba0fc69af6f0eefe05a9b56e
Cameron Yule <cameron@cameronyule.com>
2016-11-06 17:30:23 +0000

    Ensure BasketItem has a product instance

--- app/models/basket_item.rb
+++ app/models/basket_item.rb
@@ -2,4 +2,6 @@ class BasketItem
   include Mongoid::Document
   belongs_to :product
   embedded_in :basket, inverse_of: :items
+
+  validates :product, presence: true
 end
--- test/models/basket_item_test.rb
+++ test/models/basket_item_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class BasketItemTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "should require a product" do
+    basket_item = BasketItem.new
+    assert_not basket_item.valid?
+    assert_not_empty basket_item.errors.details[:product]
+  end
+
+  test "should validate with correct parameters" do
+    product = build(:product)
+    basket_item = BasketItem.new
+    basket_item.product = product
+
+    assert basket_item.valid?
+  end
+
 end
